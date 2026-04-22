a8b1edd30dd0a7b155345f5106fa487d1ea2239f
tonychg <tonychg7@pm.me>
2026-04-22 14:57:00 +0200

    feat: bootstrap local kind cluster

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          kind
+          kustomize
+          kustomize-sops
+          kubernetes-helm
+          kubectl
+          kube-capacity
+          argocd
+          k9s
+          gnumake
+          cilium-cli
+          traceroute
+          go-task
+        ];
+      };
+    });
+}
